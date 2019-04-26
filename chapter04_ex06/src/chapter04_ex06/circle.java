52b15f4e5344b0d5dc851aed81ceeef278e533a2
USER <USER@AMP-27>
2019-04-26 11:50:06 +0900

    sdfsfsf

---
+++ chapter04_ex06/src/chapter04_ex06/circle.java
@@ -0,0 +1,13 @@
+package chapter04_ex06;
+
+public class circle {
+	int radius;
+	public circle(int radius)
+	{
+		this.radius = radius;
+	}
+public double getarea()
+{
+	return 3.14*radius*radius;
+}
+}
