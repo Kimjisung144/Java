8c8c2068818df9d9adc4930ae635ea6ec72a639b
HP Note <HP Note@192.168.25.41>
2019-05-01 16:58:02 +0900

    dgdgdgd

---
+++ chapter04_ex08/src/chapter04_ex08/ArrayPrameterEx.java
@@ -0,0 +1,27 @@
+package chapter04_ex08;
+
+public class ArrayPrameterEx {
+	static void replacespace(char a[])
+	{
+		for(int i = 0; i<a.length;i++)
+		{
+			if(a[i]=='')
+				a[i]  = ',';
+		}
+	}
+	static void printCharArray(char a[])
+	{
+		for(int i=0; i<a.length; i++)
+		{
+			System.out.print(a[i]);
+			System.out.println();
+		}
+	}	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c[] = {'T','h','i','s',' ','i',};
+		printCharArray(c);
+		replacespace(c);
+		printCharArray(c);
+	}
+
+}
