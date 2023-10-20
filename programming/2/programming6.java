f2c95c74e2033a0306e333ca4d9cc84ca75ff49f
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-10-21 02:58:33 +0900

    Create programming6.java

---
+++ programming/2/programming6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("드라이브 이름: c\n");
+        System.out.printf("디렉터리 이름: \\test\\\ n");
+        System.out.printf("파일 이름 : sample\n");
+        System.out.printf("확장자:py\n");
+        System.out.printf("완전한 이름은 c:\\test\\sample.py");
+    }
+}
