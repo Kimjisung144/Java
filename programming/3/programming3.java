aa2653544b1928cba167ce9a512d27eb616a9696
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-10-21 15:00:28 +0900

    Create programming3.java

---
+++ programming/3/programming3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=50;i++)
+        {
+            if(i%3==0)
+            {
+                System.out.print("짝 ");
+            }else
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
