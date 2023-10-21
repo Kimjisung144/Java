726c839040ab291aa5cfd28228957ad5cb3fadc8
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-10-21 16:58:47 +0900

    Create programming11.java

---
+++ programming/3/programming11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String a[] = {"hello","java","world"};
+        for(String b:a)
+        {
+            System.out.println(b);
+        }
+    }
+}
