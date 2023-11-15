9231aaf76f76a4426fd2003cc96c4d2d700ec5e7
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-11-15 23:30:36 +0900

    Delete programming/programming/1 directory

--- programming/programming/1/programming1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Main
-{
-    public static void main(String args[])
-    {
-        System.out.println("자바 프로그래머 여러분");
-        System.out.println("안녕하세요?");
-    }
-}
