932edc7eb245c06e1576647aed4bf607d903cd61
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-09 19:23:58 +0900

    Create Main.java

---
+++ programming/5/programming3/Main.java
@@ -0,0 +1,12 @@
+import src.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MyMetric m1 = new MyMetric();
+        double a=m1.KiloTomile(10.0);
+        System.out.println(a);
+    }
+}
