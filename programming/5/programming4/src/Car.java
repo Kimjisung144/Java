af8e346ddf161fb7b3d5db1b723a33b18ed67753
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-09 19:40:22 +0900

    Update Car.java

--- programming/5/programming4/src/Car.java
+++ programming/5/programming4/src/Car.java
@@ -1,1 +1,22 @@
 
+package src;
+
+public class Car {
+    public String model;
+    public String make;
+    public static int number;
+    public Car()
+    {
+
+    }
+    public Car(String model,String make)
+    {
+        this.model = model;
+        this.make = make;
+        number +=1;
+    }
+    public static int getNumberOfCar()
+    {
+        return number;
+    }
+}
