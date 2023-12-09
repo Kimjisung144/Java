49b9c43c5341a23e7f90321950e45924d43cbd5f
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-09 19:22:10 +0900

    Create MyMetric.java

---
+++ programming/5/src/MyMetric.java
@@ -0,0 +1,17 @@
+package src;
+
+public class MyMetric
+{
+    public MyMetric()
+    {
+
+    }
+    public static double KiloTomile(double input_1)
+    {
+        return 0.621371 * input_1;
+    }
+    public static double mileToKilo(double input_1)
+    {
+        return 1.609344 * input_1;
+    }
+}
