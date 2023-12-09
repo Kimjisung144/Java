a673befe78409ede0f85683106952ef2034ef0ec
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-10 01:36:25 +0900

    Create Word.java

---
+++ programming/5/programming7/src/Word.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Word
+{
+    private String eng;
+    private String kor;
+    public Word()
+    {
+        this.eng = null;
+        this.kor = null;
+    }
+    public Word(String n, String n1)
+    {
+        this.eng = n;
+        this.kor = n1;
+    }
+    public String get_eng()
+    {
+        return this.eng;
+    }
+
+    public String get_kor()
+    {
+        return this.kor;
+    }
+}
