b42ec5b737f932368ac09fc4a567e9c7ed7439ea
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-09 19:41:13 +0900

    Create Main.java

---
+++ programming/5/programming4/Main.java
@@ -0,0 +1,11 @@
+import src.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Car c = new Car("pony","sket");
+        System.out.println(c.getNumberOfCar());
+    }
+}
