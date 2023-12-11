59d78dfde4cfb48aebd41b761b2e2ef72e8729c4
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-11 19:48:30 +0900

    Create Sports.java

---
+++ programming/6/programming3/src/Sports.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Sports {
+    public Sports()
+    {
+
+    }
+    public int getPlayers()
+    {
+        return 0;
+    }
+    public String getName()
+    {
+        return "아직 결정되지 않음";
+    }
+}
