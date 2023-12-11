3bde46894aa9152992e855ffb2186b3bcb562612
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-11 20:05:33 +0900

    Create ColorRectangle.java

---
+++ programming/6/programming4/src/ColorRectangle.java
@@ -0,0 +1,10 @@
+package src;
+
+public class ColorRectangle extends Rectangle{
+    public String color;
+    public ColorRectangle(int w,int h,String c)
+    {
+        super(w,h);
+        this.color = c;
+    }
+}
