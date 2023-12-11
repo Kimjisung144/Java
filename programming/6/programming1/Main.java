0faf1128701213f9c8c51b375ec24e45479b4f98
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-11 18:58:49 +0900

    Create Main.java

---
+++ programming/6/programming1/Main.java
@@ -0,0 +1,12 @@
+import src.*;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Pizza p = new Pizza(20,"pepperoni");
+        System.out.println(p);
+    }
+}
