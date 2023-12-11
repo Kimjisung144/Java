d06ec1cdc6e640ed84ba527932a8ff80add4a5f0
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-11 19:17:31 +0900

    Create Main.java

---
+++ programming/6/programming2/Main.java
@@ -0,0 +1,14 @@
+import good.*;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Bird b = new Bird();
+        b.fly();
+        b.sing();
+        b.walk();
+    }
+}
