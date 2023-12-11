01d5fbb3ab5025b623ef484c40791e0e0fdddb5e
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-11 18:57:28 +0900

    Create circle.java

---
+++ programming/6/programming1/src/circle.java
@@ -0,0 +1,13 @@
+package src;
+
+public class circle {
+    protected int radius;
+    public circle()
+    {
+        this.radius=0;
+    }
+    public circle(int r)
+    {
+        this.radius = r;
+    }
+}
