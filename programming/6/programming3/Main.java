5ae4c4f4b2d4a64633f82fe1fb8a8548d54af80f
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-11 19:50:03 +0900

    Create Main.java

---
+++ programming/6/programming3/Main.java
@@ -0,0 +1,10 @@
+import src.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Soccer s = new Soccer("축구",11);
+        System.out.println(s.toString());
+    }
+}
