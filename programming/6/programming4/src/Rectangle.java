c798c59ffb06ea4c63e6413e45f10b000b98a986
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-11 20:04:45 +0900

    Create Rectangle.java

---
+++ programming/6/programming4/src/Rectangle.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Rectangle {
+    public int width;
+    public int height;
+    public Rectangle(int w,int h)
+    {
+        this.height=h;
+        this.width=w;
+    }
+}
