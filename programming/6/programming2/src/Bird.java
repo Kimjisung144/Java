5640a95507b472b353a88da6434563adce639e2e
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2023-12-11 19:16:19 +0900

    Create Bird.java

---
+++ programming/6/programming2/src/Bird.java
@@ -0,0 +1,12 @@
+package good;
+
+public class Bird extends Animal{
+    public void fly()
+    {
+        System.out.println("날을수있음");
+    }
+    public void sing()
+    {
+        System.out.println("노래부를수있음");
+    }
+}
