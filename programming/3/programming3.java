eb6f2c8f1364f008d9063bd80610b300c233c7b2
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2024-08-15 02:27:24 +0900

    Update programming3.java

--- programming/3/programming3.java
+++ programming/3/programming3.java
@@ -1,18 +1,15 @@
-import java.util.Scanner;
-
-public class Main
-{
+// 369게임 
+public class Main {
     public static void main(String[] args)
     {
-        for(int i=1;i<=50;i++)
-        {
-            if(i%3==0)
-            {
-                System.out.print("짝 ");
-            }else
-            {
-                System.out.print(i+" ");
-            }
-        }
+      for(int i = 1; i <= 50; i++) // 1~50까지 반복
+      {
+          if(i % 3 == 0)     // 3의 배수일때
+          {
+              System.out.print("짝"+" "); // 숫자가 아닌 짝으로 하기
+          }else{
+              System.out.print(i+" ");    // 숫자 출력
+          }
+      }
     }
 }
