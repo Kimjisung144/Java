0d253c07f9795a04b1468b522841a7e1d48d7b84
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2024-08-16 23:52:06 +0900

    Update programming5.java

--- programming/3/programming5.java
+++ programming/3/programming5.java
@@ -1,18 +1,16 @@
-import java.util.Scanner;
-
-public class Main
-{
+// 두개의 주사위 합이 6일때 경우의 수 구하기
+public class Main {
     public static void main(String[] args)
     {
-        for(int i=1;i<=6;i++)
-        {
-            for(int j=1;j<=6;j++)
-            {
-                if(i+j==6)
-                {
-                    System.out.println("("+i+","+j+")\n");
-                }
-            }
-        }
+      for(int i=1; i<=6; i++) //1~6까지
+      {
+          for(int j=1; j<=6; j++) //1~6까지
+          {
+              if(i+j == 6) //두개의 주사위 합이 6일때
+              {
+                  System.out.println("("+i+", "+j+")"); //그 때 주사위 결과물들 출력
+              }
+          }
+      }
     }
 }
