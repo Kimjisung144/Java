4ac2cc0279ce67bdd214a92a61c8a6074eb75c52
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2024-08-17 00:16:00 +0900

    Update programming6.java

--- programming/3/programming6.java
+++ programming/3/programming6.java
@@ -1,18 +1,16 @@
-import java.util.Scanner;
-
-public class Main
-{
+// 3x+10y==100 모든 해 구하기
+public class Main {
     public static void main(String[] args)
     {
-        for(int i=0;i<=10;i++)
-        {
-            for(int j=0;j<=10;j++)
-            {
-                if((3*i)+(10*j)==100)
-                {
-                    System.out.println("("+i+","+j+")\n");
-                }
-            }
-        }
+      for(int i=0; i<=10; i++) //1~10까지
+      {
+          for(int j=0; j<=10; j++) //1~10까지
+          {
+              if((3*i) + (10*j) == 100) //3x+10y=100 가 참일때
+              {
+                  System.out.println("("+i+", "+j+")"); //그 때 결과물들 출력
+              }
+          }
+      }
     }
 }
