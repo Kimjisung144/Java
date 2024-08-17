7ef4c695c1d123af7c517118d8610fe0588c7875
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2024-08-17 22:13:48 +0900

    Update programming11.java

--- programming/3/programming11.java
+++ programming/3/programming11.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args)
     {
         String[] str = {"hello","java","world"}; //문자열 배열 생성하여 원하는내용으로 초기화
-        for(String e:str) // 하나하나 가져와서 변수 e에 넣음으로써 사용
+        for(String e:str)                        // 하나하나 가져와서 변수 e에 넣음으로써 사용
         {
             System.out.println(e); // 내용 출력
         }
