bc8a17983f8187d5ef1a304afb4b224b2ae19aaf
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2024-08-20 06:48:16 +0900

    Update programming12.java

--- programming/3/programming12.java
+++ programming/3/programming12.java
@@ -1,23 +1,50 @@
+//시험점수의 합계와 평균 구하기
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
+    public static void input_score(ArrayList<Integer> s) // 성적 점수 입력받는 함수
+    {
+        int temp = 0;                        // 임시로 점수 저장용 변수
+        Scanner sc = new Scanner(System.in); // 입력받기 위해 객체 생성
+
+        while(true)
+        {
+            System.out.println("성적을 입력하시오:"); // 성적 입력받기 위한 안내문
+            temp = sc.nextInt();                   // 성적 입력받기
+
+            if(temp == -1) // -1을 입력받으면 입력받는거 그만두기
+            {
+                break;     // 멈춤
+            }
+            s.add(temp);   // 점수 배열에 추가
+        }
+    }
+
+    public static int sum(ArrayList<Integer> s) // 합계 출력
+    {
+        int sum = 0;   // 합계 저장을 위한 변수
+        for(int num:s) // 하나하나 가져오기
+        {
+            sum = sum + num;  // 합계변수에 점수 더하기
+        }
+        return sum; // 더한 결과 반환하기
+    }
+
+    public static double avg(ArrayList<Integer> s,int sum) // 평균
+    {
+        double avg = 0;               // 평균 저장용 변수
+        avg = (double) sum /s.size(); // 합계 / 과목 갯수
+        return avg;                   // 평균 반환
+    }
+
     public static void main(String[] args)
     {
-        Scanner SC = new Scanner(System.in); // 스캐너 객체 생성
-        int a[] = new int[100]; // 배열 객체 생성
-        int i=1; //a배열의 인텍스 제어값
+        int score_sum = 0;                            // 합계를 저장하기 위한 변수
+        ArrayList<Integer> score = new ArrayList<>(); // 점수 저장하고 보관하기 위해 동적 배열 생성
 
-        while(a[i-1] >= 0) //
-        {
-            a[i-1] = SC.nextInt();
-            i++;
-        }
-        int sum=0;
-        for(int j=0; j<i-1; j++)
-        {
-            sum += a[i];
-        }
-        System.out.printf("합계: %d 평균:%.1f",sum,(sum/i-1.0));
+        input_score(score);                           // 점수 입력받기 위해 동적배열(주소)을 인수로 보내기
+        score_sum = sum(score);                       // 합계 구하기 위해 함수 호출
+        System.out.println("합계: "+score_sum+"\n평균: "+avg(score,score_sum)); // 합계와 평균 출력
     }
 }
