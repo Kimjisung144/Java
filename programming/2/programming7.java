f74487873868c5369e370961820d7b3c8f735193
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2025-01-04 23:35:31 +0900

    Update programming7.java

--- programming/2/programming7.java
+++ programming/2/programming7.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
-
-public class Main
-{
+public class Main{
     public static void main(String[] args)
     {
-        int pay;
-        int price;
-        Scanner sc = new Scanner(System.in);
-        pay = sc.nextInt();
-        price = sc.nextInt();
-        System.out.printf("받은 돈:%d\n",pay);
-        System.out.printf("상품 가격:%d\n",price);
-        System.out.printf("부가세 %.0f\n",price*0.1);
-        System.out.printf("잔돈:%d",pay - price);
+        int pay = 10000; //지불금액 저장 변수
+        int value = 7500; //상품가격 저장 변수
+        System.out.println("받은 돈: "+ pay); // 받은 돈 출력 
+        System.out.println("상품 가격: "+ value); //상품 가격 출력
+        System.out.println("부가세: "+(pay*0.1)); // 부가세 구하고 출력
+        System.out.println("잔돈: "+(pay-value)); //잔돈 구하고 출력
     }
 }
