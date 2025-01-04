71707818b250e29265a664b248ab2759613a9c1c
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2025-01-04 22:02:52 +0900

    Update programming6.java

--- programming/2/programming6.java
+++ programming/2/programming6.java
@@ -1,13 +1,11 @@
-import java.util.Scanner;
-
-public class Main
-{
+public class Main{
     public static void main(String[] args)
     {
-        System.out.printf("드라이브 이름: c\n");
-        System.out.printf("디렉터리 이름: \\test\\\ n");
-        System.out.printf("파일 이름 : sample\n");
-        System.out.printf("확장자:py\n");
-        System.out.printf("완전한 이름은 c:\\test\\sample.py");
+        System.out.println("드라이브 이름: c");
+        System.out.println("디렉터리 이름: \\test\\");
+        System.out.println("파일 이름: sample");
+        System.out.println("확장자: Py");
+        System.out.println();
+        System.out.println("완전한 이름은 c:\\test\\sample.py");
     }
 }
