7b3fc2c74f21d3d1d6f6d8d6ee89221c4e2c4df9
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2025-01-04 23:58:30 +0900

    Update programming8.java

--- programming/2/programming8.java
+++ programming/2/programming8.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
+import java.lang.Math;
+public class Main{
 
-public class Main
-{
+    public static double volume(double r) //구의 부피를 구하기 위한 메소드
+    {
+        return (4.0/3.0)*(Math.PI *(r*r*r)); //4/3 *파이 *r^2;
+    }
+
     public static void main(String[] args)
     {
-        double pay;
         Scanner sc = new Scanner(System.in);
-        pay = sc.nextDouble();
-        System.out.printf("구의 부피:%f",(4/3.0)*3.14*(pay*pay*pay));
+        double r; //구의 반지름 저장 변수
+        System.out.printf("구의 반지름: ");
+        r = sc.nextDouble(); //반지름 입력받기
+
+        System.out.println("구의 부피: "+volume(r));//구의 부피 출력
     }
 }
