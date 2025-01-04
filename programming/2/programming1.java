1da035c65532d3d7513a32c845920b83467eb7cd
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2025-01-04 20:44:35 +0900

    Update programming1.java

--- programming/2/programming1.java
+++ programming/2/programming1.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String args[])
+public class Main{
+
+    public static int box(int orange) // box갯수 알기위한 메소드
     {
-        Scanner sc = new Scanner(System.in);
-        int box;
-        int orange = 127;
-        System.out.printf("오렌지의 개수를 입력하시오:");
-        orange = sc.nextInt();
-        System.out.printf("%.0f 박스가 필요하고 %.0f개 남습니다.",orange/10.0,orange%10.0);
+        return (int)orange/10; //박스하나에 오렌지 10개씩 넣기
+    }
+    
+    public static int number(int orange) //오렌지 박스에 남은 오렌지 갯수 구하는 메소드
+    {
+        return (int)orange % 10; //나머지 구하기
+    }
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in); //입력받는 객체 생성
+        int orange;                          //오렌지 갯수 저장 변수
+        System.out.print("오렌지의 개수를 입력하시오:");
+
+        orange = sc.nextInt();               //오렌지 갯수 입력받는다
+        System.out.println(box(orange)+"박스가 필요하고 "+number(orange)+"개가 남습니다."); //각 메소드를 호출하여 결과 출력하기
     }
 }
