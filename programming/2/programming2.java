22d0f48c8ccb2e3df16d1fc7b7161381fe0445b5
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2025-01-04 21:03:47 +0900

    Update programming2.java

--- programming/2/programming2.java
+++ programming/2/programming2.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String args[])
+public class Main{
+
+    public static double mile_to_km(double mile) //마일에서 킬로미터로 변환
+    {
+        return (double) (mile*1.609); //식
+    }
+
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        double x;
-        System.out.printf("마일을 입력하시오:");
-        x = sc.nextInt();
-        System.out.printf("%.1f마일은 %.1f킬로미터 입니다.",x,x*1.609);
+        double mile;                      //마일 저장 변수
+        System.out.print("마일을 입력하시오:");
+
+        mile = sc.nextDouble(); //마일 입력
+        System.out.println(mile + "마일은 " + mile_to_km(mile) + " 킬로미터입니다."); //변환 메소드 호출하고 변환 결과 출력
+
     }
 }
