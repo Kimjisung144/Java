c7fced4d28c2998478d217d13f68ed4389ba6976
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2025-01-06 18:10:56 +0900

    Update programming1.java

--- programming/3/programming1.java
+++ programming/3/programming1.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        String[] st = {"one","two","three","four","five","six","seven","eigh","nine","ten","other"};   //문자열 여러개 저장을 위해 배열 생성
-        Scanner sc = new Scanner(System.in);
-        int i = 0; //입력받기 위한 변수
-        i = sc.nextInt(); // 입력받기
-        if(i > 0 && i < 10)
+public class Main
+{
+    public static void output_print(int number)// 단어로 출력하기 위한 메소드
+    {
+        String[] list = {"OTHER","ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE"}; //출력에 쓸 배열
+        if(number > 0 && number < 10) //0보다 크고 10보다 작은 숫자 일때
         {
-            System.out.println(st[i-1]); //출력
-        }else{
-            System.out.println(st[10]); //1~9까지 숫자가 아닐시 출력
+            System.out.println(list[number]);
+        }else{                       //1~9가 아닐시
+            System.out.println(list[0]);
         }
+    }
+
+    public static void main(String[] args)
+    {
+        int input_number;                     //입력받기 위한 변수
+        Scanner sc  = new Scanner(System.in); //입력받기 위한 객체 생성
+        input_number = sc.nextInt();          //입력받기
+        output_print(input_number);           //출력 하기 위한 메소드 호출
     }
 }
